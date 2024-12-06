4c839bece4c82d5632e69f23df925a68539a0fc2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-06 23:35:06 +0530

    express started

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Chai aur backend")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/twitter", (req, res) => {
+    res.send('Thoufiq dot com')
+})
+
+app.get("/login", (req, res) => {
+    res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get("/youtube", (req, res) => {
+    res.send('<h2>Chai aur code</h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
